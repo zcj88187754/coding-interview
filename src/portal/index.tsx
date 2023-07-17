ad922cb58f65c3e8adba7895d27e096af998e6dc
Yourz <crazilou@vip.qq.com>
2023-07-17 22:09:01 +0800

    fix: hotfix for chart import

--- src/portal/index.tsx
+++ src/portal/index.tsx
@@ -1,8 +1,8 @@
-import Chart from '../Echarts';
+import Chart from 'echarts-for-react';
 
 function Portal() {
   // implement the Portal here
-  return <Chart />;
+  return <Chart option={{}} />;
 }
 
 export default Portal;
